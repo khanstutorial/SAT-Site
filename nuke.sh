a3d72e368680dfc95086fd7ef87c226250a546f6
JFreyra <julius@freyra.org>
2018-10-07 19:48:09 -0400

    removed box shadow around carousel buttons, added devLog, added nuke script

---
+++ nuke.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm *.html
+rm ./nbrhoodFaqs/*.html
+rm ./nbrhoodHoF/*.html
+rm ./nbrhoodMtrshp/*.html
+rm ./nbrhoodProgs/*.html
+cp ./nukeShelter/index.html ./nukeShelter/thankyou.html .
